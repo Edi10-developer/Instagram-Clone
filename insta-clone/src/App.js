d0c1169c400ef83189241403952e26903492869a
Edi <selimi.edi.1991@gmail.com>
2021-10-21 11:21:09 +0200

    First Commit
    Project setted

---
+++ insta-clone/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+    <div className="app__header">
+      <img 
+      className="app__headerImage"
+      src="https://seeklogo.com/images/I/instagram-logo-468E0CC266-seeklogo.com.png" />
+    </div>
+    {/* Header */}
+      {/* Posts */}
+        {/* Posts */}
+    </div>
+  );
+}
+
+export default App;
