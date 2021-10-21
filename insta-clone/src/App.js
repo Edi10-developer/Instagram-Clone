86ec11749e710fd6bffb9382c0018f9ea30665c7
Edi <selimi.edi.1991@gmail.com>
2021-10-21 21:17:45 +0200

    Post Component
    Created and styled Post component

--- insta-clone/src/App.js
+++ insta-clone/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Post } from './components/index';
 
 function App() {
   return (
@@ -9,6 +10,16 @@ function App() {
       className="app__headerImage"
       src="https://seeklogo.com/images/I/instagram-logo-468E0CC266-seeklogo.com.png" />
     </div>
+    <Post 
+    username="ediselimi"
+    imageUrl="https://freefrontend.com/assets/img/css-logos/react-logo-pure-css.png" 
+    caption="Wow, il corso di React è davvero interessante."
+    />
+       <Post 
+    username="robertobaggio"
+    imageUrl="https://i1.wp.com/blog.logrocket.com/wp-content/uploads/2020/06/React-Native.png?fit=730%2C412" 
+    caption="Wow, il corso di React è davvero interessante."
+    />
     {/* Header */}
       {/* Posts */}
         {/* Posts */}
